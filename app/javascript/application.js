d15fae9f0da245f0ad776067eb1b5ae70c39d9ea
Gregory McIntyre <greg@gregorymcintyre.com>
2022-03-08 14:36:04 +1100

    Are these dots stuffing it

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,8 @@
 
 // import * as popper from 'popper'
 // import * as bootstrap from 'bootstrap'
-import './custom/checkbox'
-import './custom/time-zone-select'
+import 'custom/checkbox'
+import 'custom/time-zone-select'
 
 import Rails from '@rails/ujs'
 
