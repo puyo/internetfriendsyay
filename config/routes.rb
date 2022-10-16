18fd1667eb3d7cf5e50a738f818b7bac214b3634
Gregory McIntyre <greg@gregorymcintyre.com>
2022-10-17 09:20:30 +1100

    Redirect heroku to new site

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  constraints(host: /internetfriendsyay.herokuapp.com/) do
+    match '/(*path)' => redirect {|params, req| "https://internetfriendsyay.com/#{params[:path]}"},  via: [:get, :post]
+  end
+
   resources :schedules do
     resources :people
   end
